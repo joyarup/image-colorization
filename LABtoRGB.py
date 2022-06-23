7d4fb150d3fe417ab7a05c5ff6b49f76ec05ef22
joyarup <nfsnero@gmail.com>
2022-06-23 11:36:25 +0530

    added comments

--- LABtoRGB.py
+++ LABtoRGB.py
@@ -2,30 +2,37 @@ import math
 def lab2rgb(inputColor):
     lab = [inputColor[0],inputColor[1],inputColor[2]]
 
+    #Taking f(x,y,z) coordinates using the formula given below
+    
     y = (lab[0] + 16) / 116
     x = lab[1] / 500 + y
     z = y - lab[2] / 200
 
-
+    #xr, yr, zr, values calculations
+    
     if x ** 3 > 0.008856:
-        x = (x ** 3) * 0.95047
+        x = (x ** 3) * 0.95047  #D65 white ref_X
     else:
         x = (x - 16/116) / 7.787
 
     if lab[0] > 7.999:
-        y = (y ** 3)
+        y = (y ** 3)            #D65 white ref_Y (1)
     else:
         y = lab[0] / 903.3
 
     if z ** 3 > 0.008856:
-        z = (z ** 3) * 1.08883
+        z = (z ** 3) * 1.08883  #D65 white ref_Z
     else:
         z = (z - 16/116) / 7.787
 
+    #Inverse Transformation Matrix Multiplication to get linearRGB
+    
     r = x * 3.2406 + y * -1.5372 + z * -0.4986
     g = x * -0.9689 + y * 1.8758 + z * 0.0415
     b = x * 0.0557 + y * -0.2040 + z * 1.0570
-
+    
+    #Inverse Gamma function companding for RGB
+    
     if r > 0.0031308:
         r = (1.055 * math.pow(r, 1/2.4) - 0.055)
     else:
@@ -39,7 +46,8 @@ def lab2rgb(inputColor):
     else:
         b = b * 12.92
 
-
+    #Returning 1-255 range
+    
     return [max(0, min(1, r)) * 255,
             max(0, min(1, g)) * 255,
             max(0, min(1, b)) * 255]
